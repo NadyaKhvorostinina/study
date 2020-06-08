f0647459c1467e72deca12a2d52b3c3eb934a93e
Aleksey Antufev <antufev@yamoney.ru>
2020-06-08 23:44:06 +0300

    3 commit

--- Example.java
+++ Example.java
@@ -1,5 +1,11 @@
 public class A {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
     }
 }
